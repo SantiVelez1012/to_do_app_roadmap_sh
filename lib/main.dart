6b0ddb9e8e5c52b10e37980be2e950822139d879
Santiago Velez <santivekez1012@gmail.com>
2024-10-23 10:45:24 -0500

    deleting not used compilations

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: Scaffold(
+        body: Center(
+          child: const Text("Hola"),
+        ),
+      ),
         
     );
   }
